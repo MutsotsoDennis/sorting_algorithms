1cac251aa4e22c2d479f093d4e30adcce8831f1a
MutsotsoDennis <mutsotsodennis90@gmail.com>
2024-01-24 05:33:37 -0800

    Msatse

--- print_arrei.c
+++ print_arrei.c
@@ -5,14 +5,14 @@
  * print_arrei - Prints an array of integers
  *
  * @arrei: The array to be printed
- * @sise: Number of elements in @array
+ * @leng: Number of elements in @array
  */
-void print_arrei(const int *arrei, size_t sise)
+void print_arrei(const int *arrei, size_t leng)
 {
 	size_t q;
 
 	q = 0;
-	while (arrei && q < sise)
+	while (arrei && q < leng)
 	{
 		if (q > 0)
 			printf(", ");
